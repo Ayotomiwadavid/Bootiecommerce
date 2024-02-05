ff61bdf91bcf90a444f7baf4b38d4901b10a78fa
Ayotomiwa David <olusanyatomiwa97@gmail.com>
2024-02-05 02:24:25 -0800

    first commit in this branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "./logo"
 import Navitem from './Navitem'
+import './header.css'
 
 function Header() {
+  let [showSidebar, setShowSidebar] = useState(false)
+  let handleClick = () =>{
+    setShowSidebar(!showSidebar);
+  }
   return (
     <header className='header'>
     <Logo />
-    <Navitem />
+    <Navitem 
+      showSidebar={showSidebar}
+    />
+    <i className={showSidebar ? 'fa fa-times menuIcon': 'fa fa-bars menuIcon'} onClick={handleClick}></i>
     </header>
   )
 }
--- src/components/Navitem.js
+++ src/components/Navitem.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import './header.css'
 
 function Navitem() {
   return (
     <ul className='navList'>
     <li className='listItem'>about</li>
     <li className='listItem'>home</li>
-    <li className='listItem'>home</li>
     <li className='listItem'>blog</li>
-    <li className='listItem'>categories</li>
     <li className='listItem'>contact</li>
+    <li className='listItem'>categories</li>
     </ul>
   )
 }
