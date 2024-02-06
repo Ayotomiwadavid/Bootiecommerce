573b189f7b33a364ec87223e0552c4dc1c68e1f6
unknown <olusanyatomiwa97@gmail.com>
2024-02-06 01:46:26 -0800

    update responsiveness and react dom routers and pages

--- src/components/Navitem.js
+++ src/components/Navitem.js
@@ -2,15 +2,22 @@ import React from 'react'
 import './header.css'
 import {Link} from 'react-router-dom';
 
-function Navitem() {
+function Navitem(props) {
+  let showSidebar = props.showSidebar;
   return (
-    <ul className='navList'>
-    <Link className='navlink active' to='/'><li className='listItem'>home</li></Link>
+    <ul className='navList' style={{right: showSidebar ? "0" : "-100%"}}>
+    <Link className='navlink' to='/'><li className='listItem'>home</li></Link>
     <Link className='navlink' to='/about'><li className='listItem'>about</li></Link>
     <Link className='navlink' to='/blog'><li className='listItem'>blog</li></Link>
     <Link className='navlink' to='/contact'><li className='listItem'>contact</li></Link>
-    <ul className='listItem'>categories
-    </ul>
+    <li className='listItem categorybutton'>dropdown
+      <ul className='subNavLinkCont'>
+        <Link className='navlink' to='/product'><li className='subNavLink'>product</li></Link>
+        <Link className='navlink' to='/cart'><li className='subNavLink'>cart</li></Link>
+        <Link className='navlink' to='/signup'><li className='subNavLink'>sign up</li></Link>
+        <Link className='navlink' to='/signin'><li className='subNavLink'>sign in</li></Link>
+      </ul>
+    </li>
     </ul>
   )
 }
